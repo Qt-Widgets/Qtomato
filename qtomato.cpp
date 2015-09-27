a8adafc364e3b19804c34a3764948ce5b4c7ee33
Md Minhazul Haque <mdminhazulhaque@gmail.com>
2015-09-27 18:18:55 +0600

    Absquatulate

--- qtomato.cpp
+++ qtomato.cpp
@@ -6,9 +6,87 @@ Qtomato::Qtomato(QWidget *parent) :
     ui(new Ui::Qtomato)
 {
     ui->setupUi(this);
+
+    pomodoroTimerElpsed = 0;
+
+    threadTimer.setInterval(1000);
+    pomodoroTimer.setInterval(25*60*1000);
+
+    connect(&threadTimer, SIGNAL(timeout()), this, SLOT(processTimer()));
+
+    ui->buttonStart->setDisabled(true);
+    ui->buttonStop->setDisabled(true);
+    ui->buttonReset->setDisabled(true);
+
+    processTimer();
+    threadTimer.start();
 }
 
 Qtomato::~Qtomato()
 {
+    if(pomodoroTimer.isActive())
+        pomodoroTimer.stop();
+
+    if(threadTimer.isActive())
+        threadTimer.stop();
+
     delete ui;
+}
+
+void Qtomato::on_buttonPomodoro_clicked()
+{
+    pomodoroTimer.start();
+
+    ui->buttonStop->setEnabled(true);
+    ui->buttonReset->setEnabled(true);
+}
+
+void Qtomato::on_buttonShortBreak_clicked()
+{
+
+}
+
+void Qtomato::on_buttonLongBreak_clicked()
+{
+
+}
+
+void Qtomato::on_buttonStart_clicked()
+{
+    pomodoroTimer.start(pomodoroTimerElpsed);
+    ui->buttonStart->setDisabled(true);
+    ui->buttonStop->setEnabled(true);
+}
+
+void Qtomato::on_buttonStop_clicked()
+{
+    if(pomodoroTimer.isActive())
+    {
+        pomodoroTimerElpsed = pomodoroTimer.remainingTime();
+        pomodoroTimer.stop();
+
+        ui->buttonStart->setEnabled(true);
+        ui->buttonStop->setDisabled(true);
+    }
+}
+
+void Qtomato::on_buttonReset_clicked()
+{
+    pomodoroTimer.stop();
+    pomodoroTimerElpsed = 0;
+    ui->time->setText("25:00");
+
+    ui->buttonStart->setDisabled(true);
+    ui->buttonStop->setDisabled(true);
+    ui->buttonReset->setDisabled(true);
+}
+
+void Qtomato::processTimer()
+{
+    if(pomodoroTimer.isActive())
+    {
+        QString timeText = QDateTime::fromTime_t(pomodoroTimer.remainingTime()/1000)
+                .toUTC().toString("mm:ss");
+        ui->time->setText(timeText);
+    }
 }
--- qtomato.h
+++ qtomato.h
@@ -2,6 +2,9 @@
 #define QTOMATO_H
 
 #include <QDialog>
+#include <QTimer>
+#include <QDebug>
+#include <QTime>
 
 namespace Ui {
 class Qtomato;
@@ -15,8 +18,21 @@ public:
     explicit Qtomato(QWidget *parent = 0);
     ~Qtomato();
 
+private slots:
+    void on_buttonPomodoro_clicked();
+    void on_buttonShortBreak_clicked();
+    void on_buttonLongBreak_clicked();
+    void on_buttonStart_clicked();
+    void on_buttonStop_clicked();
+    void on_buttonReset_clicked();
+
+    void processTimer();
+
 private:
     Ui::Qtomato *ui;
+    QTimer pomodoroTimer;
+    QTimer threadTimer;
+    qint64 pomodoroTimerElpsed;
 };
 
 #endif // QTOMATO_H
